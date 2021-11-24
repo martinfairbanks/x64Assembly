d8ff1c232ceb98deca9352b8ed6888824525465d
martinfairbanks <martinfairbanks@users.noreply.github.com>
2021-11-24 14:52:26 +0100

    mkdir in build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo off
+if not exist build mkdir build
 pushd build
     ml64 ../code/hello_world_console.asm /link /subsystem:console /defaultlib:kernel32.lib /entry:main
 	REM ml64 test.asm /link /subsystem:windows /defaultlib:kernel32.lib /defaultlib:user32.lib /entry:main
